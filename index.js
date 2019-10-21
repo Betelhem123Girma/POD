36b3d5da03e6936e3e2ba1f159bc4eef3fcbc7e0
betyG <betelhemgg4@gmail.com>
2019-10-21 06:28:59 -0700

    Express setup

---
+++ index.js
@@ -0,0 +1,7 @@
+var express=require('express')
+var app=express()
+var authRoute=require('./routes/auth')
+app.use('/api/user',authRoute)
+app.listen(3000,()=>{
+    console.log("Server running on port 3000")
+})
