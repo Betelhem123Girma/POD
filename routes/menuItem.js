dad29d49f4966d2345687a7e47f45fd5ce273f85
betyG <betelhemgg4@gmail.com>
2019-10-28 16:51:58 -0700

    change to package.json

--- routes/menuItem.js
+++ routes/menuItem.js
@@ -1,8 +1,9 @@
 var router=require('express').Router()
 var menuItemController=require('../controller/menuItem');
+const checkAuth = require('../lib/check_auth');
 router.get('/menuItems',menuItemController.findMenuItems)
 router.post('/addMenuItems',menuItemController.createMenuItem)
-router.get('/searchMenuItem',menuItemController.searchMenuItem)
+router.get('/searchMenuItem',checkAuth,menuItemController.searchMenuItem)
 
 // router.patch('/orderMenuItem',menuItemController.orderMenuItem)
 // router.patch('/pay',menuItemController.payMenuItem)
